24b909e671a4ecd7c762113e9c29e62ccb3cb1ad
chakeur oussama <chakeuroussama@gmail.com>
2025-06-17 12:17:59 +0800

    hia

--- alx_travel_app/README.md
+++ alx_travel_app/README.md
@@ -1,1 +1,18 @@
-this is readme 
+## ALX Travel App 0x01
+
+### Endpoints
+
+- `/api/listings/` - CRUD for Listings
+- `/api/bookings/` - CRUD for Bookings
+- `/swagger/` - Swagger UI for API docs
+
+### Tools
+
+- Django REST Framework
+- drf-yasg for Swagger
+- Postman for testing
+
+### To Run:
+
+```bash
+python manage.py runserver
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('listings.urls')),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Travel App API",
+      default_version='v1',
+      description="API for Listings and Bookings",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
