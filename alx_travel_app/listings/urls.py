9b7103c34183542cf15d502030842298bec221de
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-06-28 18:17:55 +0800

    Update urls.py

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ListingViewSet, BookingViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'listings', ListingViewSet)
@@ -8,4 +9,8 @@ router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path("initiate-payment/", views.initiate_payment, name="initiate_payment"),
+    path("verify-payment/", views.verify_payment, name="verify_payment"),
 ]
+
+
