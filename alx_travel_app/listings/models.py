7e70070e250af066a7148027dd6562aaed267ba0
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-06-28 18:15:46 +0800

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -28,3 +28,18 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.reviewer_name} - {self.rating}/5"
+        
+class Payment(models.Model):
+    booking_reference = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=[
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Failed', 'Failed')
+    ], default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.booking_reference} - {self.status}"
+
